8c363cfcf506eac391f2f153acff272ae5df47a1
shuhan <2472520650@qq.com>
2018-04-02 11:23:41 +0800

    section1/PracticeB.java

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,17 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
-    List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+  List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
+    //实现练习要求，并改写该行代码。
+    List list1 = new ArrayList(collection1);
+    List<String> list2 = new ArrayList<>();
+    for(List<String> strings : collection2){
+      list2.addAll(strings);
     }
+    list1.retainAll(list2);
+    return list1;
+  }
 }
